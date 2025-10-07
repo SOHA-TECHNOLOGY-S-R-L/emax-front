5631eee0974aa91aed3cacd933aca37eca3c5888
Bhernard Beisaga <bbeisaga@gmail.com>
2025-10-07 08:02:44 -0500

    Migracion de angular core

--- src/main.server.ts
+++ src/main.server.ts
@@ -1,4 +1,4 @@
-import { bootstrapApplication } from '@angular/platform-browser';
+import { bootstrapApplication, BootstrapContext } from '@angular/platform-browser';
 import { AppComponent } from './app/app.component';
 import { config } from './app/app.config.server';
 import localeEsPE from '@angular/common/locales/es-PE';
@@ -6,7 +6,7 @@ import localePEExtra from '@angular/common/locales/extra/es-PE';
 
 import { registerLocaleData } from '@angular/common';
 
-const bootstrap = () => bootstrapApplication(AppComponent, config);
+const bootstrap = (context: BootstrapContext) => bootstrapApplication(AppComponent, config, context);
 registerLocaleData(localeEsPE,'es-PE', localePEExtra);
 
 export default bootstrap;
