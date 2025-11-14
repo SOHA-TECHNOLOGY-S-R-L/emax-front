396485fd5f4972ed652c415b41bba9ee80b8eccf
Bhernard Beisaga <bbeisaga@gmail.com>
2025-11-13 19:02:31 -0500

    Se cambia el apunte de endpoint

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,7 +1,7 @@
 export const environment = {
-  apiLogin: `https://personas.grafiya.com.pe/login`,
-  apiUrl: `https://personas.grafiya.com.pe/api`,
-  API_URL_VER_IMAGEN: `https://personas.grafiya.com.pe/api/medios/imagen/`,
+  apiLogin: `https://clientes.grafiya.com.pe/login`,
+  apiUrl: `https://clientes.grafiya.com.pe/api`,
+  API_URL_VER_IMAGEN: `https://clientes.grafiya.com.pe/api/medios/imagen/`,
   URL_WHATSAPP_VENTA: `https://wa.me/+51978663071?text=`, //llamar a la url de sta manera https://wa.me/+974798814?text=${encodeUri(mensaje)}
   URL_WHATSAPP_CAJA: `https://wa.me/+51978663071?text=`, //llamar a la url de sta manera https://wa.me/+974798814?text=${encodeUri(mensaje)}
  // API_URL_WHATSAPP: `https://wa.me/+51978663071?text=`, //llamar a la url de sta manera https://wa.me/+974798814?text=${encodeUri(mensaje)}
